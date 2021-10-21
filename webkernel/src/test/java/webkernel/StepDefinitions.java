07e86f1e538078093c1bf82431e11715fba24b1e
ivwall <ivwall@tgndomains.com>
2021-10-21 06:07:52 -0700

    going through https://www.guru99.com/using-cucumber-selenium.html incrementally

--- webkernel/src/test/java/webkernel/StepDefinitions.java
+++ webkernel/src/test/java/webkernel/StepDefinitions.java
@@ -37,6 +37,8 @@ public class StepDefinitions {
         }
     }
 
+    WebDriver driver;
+
     @Given("today is {string}")
     public void today_is(String today) {
         this.today = today;
@@ -57,21 +59,23 @@ public class StepDefinitions {
         driver.get("https://www.google.com");
     }
 
+    // An 
+    // https://www.guru99.com/using-cucumber-selenium.html
     @Given("^Open the Firefox and launch the application$")				
-    public void open_the_Firefox_and_launch_the_application() throws Throwable							
-    {		
+    public void open_the_Firefox_and_launch_the_application() throws Throwable {		
         System.out.println("This Step open the Firefox and launch the application.");					
-    }		
+        System.setProperty("webdriver.gecko.driver", "E://Selenium//Selenium_Jars//geckodriver.exe");					
+       driver= new FirefoxDriver();					
+       driver.manage().window().maximize();			
+       driver.get("http://demo.guru99.com/v4");	   }		
 
     @When("^Enter the Username and Password$")					
-    public void enter_the_Username_and_Password() throws Throwable 							
-    {		
+    public void enter_the_Username_and_Password() throws Throwable {		
        System.out.println("This step enter the Username and Password on the login page.");					
     }		
 
     @Then("^Reset the credential$")					
-    public void Reset_the_credential() throws Throwable 							
-    {    		
+    public void Reset_the_credential() throws Throwable {    		
         System.out.println("This step click on the Reset button.");					
     }	
     
